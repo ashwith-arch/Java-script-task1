53c122d4446718265ededf959fc0bf45ba759bb7
ashwith-arch <ashwith@paithacs.com>
2026-05-27 05:18:36 -0700

    Update script.js

--- js task/script.js
+++ js task/script.js
@@ -151,3 +151,161 @@ while (count >= 1) {
     document.write(count);
     count--;
 }
+
+// --------------------day 2 task----------------------
+
+
+
+// --------switch case ----------------
+let day = parseInt(prompt("Enter the number between 1 to 7"));
+let myanswer;
+
+switch(day){
+
+    case 1:
+        myanswer = "Sunday";
+        break;
+
+    case 2:
+        myanswer = "Monday";
+        break;
+
+    case 3:
+        myanswer = "Tuesday";
+        break;
+
+    case 4:
+        myanswer = "Wednesday";
+        break;
+
+    case 5:
+        myanswer = "Thursday";
+        break;
+
+    case 6:
+        myanswer = "Friday";
+        break;
+
+    case 7:
+        myanswer = "Saturday";
+        break;
+
+    default:
+        myanswer = "None of the above";
+        break;
+}
+
+document.write(myanswer);
+
+// -----------------square number--------------
+
+function suquare(num){
+     return num*num
+}
+document.write(suquare(8))
+
+
+ // convert the square and cube functions they should in all formats (function expression, declartion, arrow functions
+
+
+// -----------square function---------
+
+function square(num){
+    return num*num
+}
+document.write(square(8))
+
+
+function cube(num){
+    return num*num*num
+}
+document.write(cube(8))
+
+
+// --------------function expression---------
+
+let square = function(num){
+    return num*num
+}
+document.write(square(9))
+
+
+let cube =function(num){
+    return num*num*num
+}
+document.write(cube(10))
+
+// --------------Arrow function-----------
+
+let square =(num) =>{
+    return num*num
+}
+document.write(square(18))
+
+let cube =(num) =>{
+    return num*num*num
+
+}
+document.write(cube(19))
+
+ // Create an array with names and greet each name using a function/
+
+let names = ['rama','sita','laxshmana','arjuna'];
+
+function greet(name){
+    document.write("Hello " + name + "<br>");
+}
+
+for ( let i=0;  i <names.length; i++){
+    greet(names[i])
+}
+
+
+
+// -------SWITCH CASE---------------
+function checkday(day){
+
+    let myanswer;
+
+    switch(day){
+
+        case 1:
+            myanswer = "Sunday";
+            break;
+
+        case 2:
+            myanswer = "Monday";
+            break;
+
+        case 3:
+            myanswer = "Tuesday";
+            break;
+
+        case 4:
+            myanswer = "Wednesday";
+            break;
+
+        case 5:
+            myanswer = "Thursday";
+            break;
+
+        case 6:
+            myanswer = "Friday";
+            break;
+
+        case 7:
+            myanswer = "Saturday";
+            break;
+
+        default:
+            myanswer = "Invalid Number";
+    }
+
+    document.write(myanswer);
+}
+
+checkday(4);
+
+
+
+
